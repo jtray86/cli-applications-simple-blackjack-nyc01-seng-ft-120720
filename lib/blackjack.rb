797f94f42093e2e8ca26ee822371b55f25f85d7a
Jen Tracy <jenniferetracy@gmail.com>
2020-12-01 21:47:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(card_total)
     card_total
   elsif answer =="h"
    deal_card + card_total
-  elsif answer != "s"or"h"
+  else answer != "s"or"h"
    invalid_command
    prompt_user
   end 
