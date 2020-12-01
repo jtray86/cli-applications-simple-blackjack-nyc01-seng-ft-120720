0132d32001df726153bb41cfbb51cb62c790193c
Jen Tracy <jenniferetracy@gmail.com>
2020-12-01 20:17:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,8 +7,8 @@ def deal_card
   
 end
 
-def display_card_total
-  puts "Your cards add up to #{card total}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
