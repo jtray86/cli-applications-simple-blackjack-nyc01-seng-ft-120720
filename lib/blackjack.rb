35b79b58abad94be21ad5e0b8ac55eaf62ced1c6
Jen Tracy <jenniferetracy@gmail.com>
2020-12-01 21:09:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,8 +29,14 @@ def initial_round
     sum
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  answer = get_user_input
+  if answer = "s"
+    deal_card
+    
+  end
+  
 end
 
 def invalid_command
