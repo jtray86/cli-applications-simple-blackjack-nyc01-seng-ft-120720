6c462c8a43f85c1b5352344d1da40a4ff1298d69
Jen Tracy <jenniferetracy@gmail.com>
2020-12-01 21:13:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,11 +32,10 @@ end
 def hit?(card_total)
   prompt_user
   answer = get_user_input
-  if answer = "s"
-    deal_card
-    
+  if answer = "h"
+    deal_card += card_total
   end
-  puts "Please enter a valid command"
+puts "Please enter a valid command"
 end
 
 def invalid_command
